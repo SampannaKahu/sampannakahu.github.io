011b53a4785bea8e88315fea156b0e145e2d0b87
sampanna.kahu <sampanna.kahu@flipkart.com>
2016-06-09 21:37:07 +0530

    Adding sidebar made using Semantic-UI.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function showSidebar() {
+    $('.ui.labeled.icon.sidebar')
+    //.sidebar('setting', 'transition', 'scale down')
+        .sidebar('setting', 'transition', 'overlay')
+        .sidebar('show')
+    ;
+}
+
+function toggleSidebar() {
+    $('.ui.labeled.icon.sidebar')
+        .sidebar('setting', 'transition', 'overlay')
+        .sidebar('toggle')
+    ;
+}
+
+
+function initialize() {
+
+    showSidebar();
+
+    $('#menuButton')
+        .sticky({
+            context: '#sideBar'
+        });
+
+    $('#menuButton').click(function () {
+        $('.ui.labeled.icon.sidebar')
+            .sidebar('setting', 'transition', 'overlay')
+            .sidebar('toggle')
+        ;
+    });
+
+}
+
+window.onload = initialize();
+
+
